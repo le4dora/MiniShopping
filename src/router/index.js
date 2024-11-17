965756de237cdccdc0feae15c592a7e0a7cc2dd0
ChenKailin <hello@123.com>
2024-11-17 20:09:00 +0800

    路由导航/商品首页/商品详情页开发

--- src/router/index.js
+++ src/router/index.js
@@ -3,6 +3,11 @@ import VueRouter from 'vue-router'
 import Home from '../views/Home.vue'
 import Login from '../views/Login.vue'
 import Register from '../views/Register.vue'
+import HomePage from '../views/HomePage/HomePage.vue'
+import Products from '../views/Products/Products.vue'
+import ShopCar from '../views/ShopCar/ShopCar.vue'
+import ProductDetail from '../views/ProductDetail/ProductDetail.vue'
+
 
 Vue.use(VueRouter)
 
@@ -26,7 +31,35 @@ const routes = [
     path: '/about',
     name: 'about',
     component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
-  }
+  },
+  {
+    path: '/Homepage',
+    name: 'Homepage',
+    component: HomePage,
+    children: [
+      {
+        path: '',//默认路由
+        component: Products,  //默认展示商品页
+        name:Products
+      },
+      {
+        path: 'Products',//商品页路由
+        component: Products,  //展示商品页
+        name:Products
+      },
+      {
+        path: 'Shopcar',//购物车路由
+        component: ShopCar,  //购物车页
+        name:ShopCar
+      },
+      {
+        path: 'Productdetail/:id',//商品详情页路由
+        component: ProductDetail,  //商品详情页
+        name:ProductDetail
+      },
+
+    ]
+  },
 ]
 
 const router = new VueRouter({
